82fd0b64082e189d4f05f5e51326021e4e142c6a
kai <sea.k.naka@gmail.com>
2021-04-05 14:40:49 +0900

    before sp-booking

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(function() {
+$(function () {
 
   //ハンバーガーメニュー開閉
-  $('.ham').on('click', function() {
-    $('.header-sp-nav, .ham, .ham-line1, .ham-line2, .ham-line3').toggleClass('open');
+  $('.ham').on('click', function () {
+    $('.header-sp-menu, .ham, .ham-line1, .ham-line2, .ham-line3').toggleClass('open');
   });
 
   //ハンバーガーメニューのナビリンククリック後自動で閉じる
@@ -11,8 +11,41 @@ $(function() {
   });
 
   //スクロールしたらヘッダーが変わる
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
     header.classList.toggle('scroll-nav', window.scrollY > 0);
+  });
+
+  //宿泊予約モーダル(PC)
+  $('.js-modal-open').on('click', function () {
+    $('.js-modal').fadeIn();
+    return false;
+  });
+  $('.js-modal-close').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
+
+  //宿泊予約モーダル(SP)
+  $('.js-modal-sp-open').on('click', function () {
+    $('.js-modal-sp').fadeIn();
+    return false;
+  });
+  $('.js-modal-sp-close').on('click', function () {
+    $('.js-modal-sp').fadeOut();
+    $('.modal-sp-content-box').on('click', function (e) {
+      e.stopPropagation();
+    });
+    return false;
+  });
+
+  //宿泊予約を押したらハンバーガーメニュー消す(SP)
+  $('.js-modal-sp-open').on('click', function () {
+    $('.ham').fadeOut();
+    return false;
+  });
+  $('.js-modal-sp-close').on('click', function () {
+    $('.ham').fadeIn();
+    return false;
   });
 });
